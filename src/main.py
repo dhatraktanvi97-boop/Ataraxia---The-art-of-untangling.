0ab0615c7a7ddcb11d1f92cc08439b3736301c30
Tanvi Dhatrak <dhatraktanvi97@gmail.com>
2026-08-28 12:19:59 +0000

    Connect analysis to responce generation

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from ataraxia.safety import validate_response, get_safe_fallback
 def run_ataraxia(user_message: str) -> str:
     context = create_context(user_message)
 
-    response = generate_response(context.user_message)
+    response = generate_response(context.analysis)
 
     safety_result = validate_response(response)
 
